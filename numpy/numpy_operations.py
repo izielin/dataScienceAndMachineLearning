f89c2c528f05168e5c8ea5a4b5f41b640258d98f
izielin <misiopar@wp.pl>
2020-07-27 15:43:46 +0200

    numpy

---
+++ numpy/numpy_operations.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+arr = np.arange(1, 11)
+print(arr + arr)
+print(arr - arr)
+print(arr * arr)
+print(arr / arr)
+
+print(arr + 100)
+
+print(np.sqrt(arr))
+print(np.max(arr))
+print(np.cos(arr))
